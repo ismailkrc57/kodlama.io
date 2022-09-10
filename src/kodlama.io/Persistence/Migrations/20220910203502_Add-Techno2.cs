3661d994dfa30fa10c12a57a509d8f51a4e93fc9
ismail.karaca <ismailkrc57@gmail.com>
2022-09-11 01:16:27 +0300

    technology added

---
+++ src/kodlama.io/Persistence/Migrations/20220910203502_Add-Techno2.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class AddTechno2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Technologies",
+                columns: new[] { "Id", "Name", "ProgramingLanguageId" },
+                values: new object[] { 1, "Spring", 1 });
+
+            migrationBuilder.InsertData(
+                table: "Technologies",
+                columns: new[] { "Id", "Name", "ProgramingLanguageId" },
+                values: new object[] { 2, "Jsp", 1 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Technologies",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Technologies",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
